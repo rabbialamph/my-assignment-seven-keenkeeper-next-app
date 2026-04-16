7802270e9ed2953aea293228afeeb72ef2a89c4f
rabbialamph <mdrabbialom98@gmail.com>
2026-04-16 17:51:08 +0600

    banner created

---
+++ src/Components/Homepage/Banner.jsx
@@ -0,0 +1,20 @@
+const Banner = () => {
+    return (
+        <div>
+            <div className="hero">
+             <div className="hero-content text-center">
+               <div className="">
+                 <h1 className="text-3xl font-semibold">Friends to keep close in your life</h1>
+                 <p className="py-6">
+                  Your personal shelf of meaningful connections. Browse, tend, and nurture the <br />
+                  relationships that matter most.
+                 </p>
+                 <button className="btn bg-green-700 text-white"> + Add a Friend</button>
+               </div>
+             </div>
+           </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
+import Banner from "@/Components/Homepage/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
-     <div>
-      
+     <div className="bg-base-200 p-5">
+       <Banner />
      </div>
   );
 }
